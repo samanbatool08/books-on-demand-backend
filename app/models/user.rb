da7337b2a1a463ffd7b0fc676808cc304e1d22e8
Saman Batool <samanbatool08@gmail.com>
2022-12-20 11:44:50 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false }
+  
+  EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false }, 
+                    format: { with: EMAIL_REGEX}
+
+
+
+
+
 end
